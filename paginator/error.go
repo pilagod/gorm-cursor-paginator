d8cbe79e2cd05af5ab37529d66f7dfe14434e45c
pilagod <pilagooood@gmail.com>
2021-05-03 20:59:02 +0800

    refine invalid limit error wording

--- paginator/error.go
+++ paginator/error.go
@@ -4,6 +4,6 @@ import "errors"
 
 // Errors for paginator
 var (
-	ErrInvalidLimit = errors.New("limit should be greater than or equal to 0")
+	ErrInvalidLimit = errors.New("limit should be greater than 0")
 	ErrInvalidOrder = errors.New("order should be ASC or DESC")
 )
